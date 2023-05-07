6da0ae9dae3248c152ec7f7fd566a4a2c57f2d79
Erard Valdez <valdez.erardtimothy@gmail.com>
2023-05-07 11:16:21 +0800

    Login UI changes
    - hide the Action Bar on AuthActivity
    - adjust vertical bias of grouped views in fragment_login

--- app/src/main/java/com/example/yieldmobile/AuthActivity.kt
+++ app/src/main/java/com/example/yieldmobile/AuthActivity.kt
@@ -9,5 +9,6 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        supportActionBar?.hide()
     }
 }
